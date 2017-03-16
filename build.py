5a92297189b4e7971d9bf9237b9a016cf1bf4bc4
MinusKelvin <minuskelvin.carlson@gmail.com>
2017-03-16 17:16:02 +1100

    Improved build script

--- build.py
+++ build.py
@@ -77,7 +77,7 @@ def buildClasses(thingToDo=None):
 		classpath += os.pathsep
 		classpath += lib
 	classpath = classpath[1:]
-	command = ["javac", "-d", "out/classes/main", "-cp", classpath]
+	command = ["javac", "-d", "out"+os.sep+"classes"+os.sep+"main", "-cp", classpath]
 	for f in sources:
 		command.append(f)
 	issueCommand(command)
@@ -93,25 +93,25 @@ def buildTestClasses(thingToDo=None):
 	print "Found {} test source files".format(len(sources))
 	libraries = find("lib", ".jar", ["javadoc.jar", "sources.jar", "natives-windows.jar", "natives-linux.jar"])
 	print "Found {} libraries".format(len(libraries))
-	classpath = "out/classes/main"
+	classpath = "out"+os.sep+"classes"+os.sep+"main"
 	for lib in libraries:
 		classpath += os.pathsep
 		classpath += lib
-	command = ["javac", "-d", "out/classes/test", "-cp", classpath]
+	command = ["javac", "-d", "out"+os.sep+"classes"+os.sep+"test", "-cp", classpath]
 	for f in sources:
 		command.append(f)
 	issueCommand(command)
 tasks["buildTestClasses"] = buildTestClasses
 
-def jar(thingToDo=None):
+def binaryJar(thingToDo=None):
 	if thingToDo == "description":
-		return "Builds the library jars. These jars do no include dependencies."
+		return "Builds the binary jar. This jar does not include dependencies."
 	if thingToDo == "dependencies":
 		return ["buildClasses"]
-	if os.path.exists("out/binJar"):
-		shutil.rmtree("out/binJar")
+	if os.path.exists("out"+os.sep+"binJar"):
+		shutil.rmtree("out"+os.sep+"binJar")
 	print "Copying {} to {}".format(os.path.join("out","classes","main"), os.path.join("out","binJar"))
-	shutil.copytree("out/classes/main", "out/binJar")
+	shutil.copytree("out"+os.sep+"classes"+os.sep+"main", "out/binJar")
 	files = find("src", excludeSuffix=[".java"])
 	for i, path in enumerate(files):
 		files[i] = path[4:] # Sorry for the magic number :(
@@ -120,18 +120,64 @@ def jar(thingToDo=None):
 		print "Copying {} to {}".format(os.path.join("src",f), os.path.join("out","binJar",f))
 		ensureDirectoryExists(os.path.join("out"+os.path.sep+"binJar",os.path.dirname(f)))
 		shutil.copy2(os.path.join("src", f), os.path.join("out"+os.path.sep+"binJar",f))
-	issueCommand(["jar", "cf", "out/mtk.jar", "-C", "out/binJar/", "."])
-tasks["jar"] = jar
+	issueCommand(["jar", "cf", "out"+os.sep+"mtk.jar", "-C", "out"+os.sep+"binJar", "."])
+tasks["binaryJar"] = binaryJar
+
+def sourceJar(thingToDo=None):
+	if thingToDo == "description":
+		return "Builds the source jar."
+	if thingToDo == "dependencies":
+		return []
+	if os.path.exists("out"+os.sep+"sourceJar"):
+		shutil.rmtree("out"+os.sep+"sourceJar")
+	files = find("src", ".java")
+	for i, path in enumerate(files):
+		files[i] = path[4:] # Sorry for the magic number :(
+	print "Found {} source files".format(len(files))
+	for f in files:
+		print "Copying {} to {}".format(os.path.join("src",f), os.path.join("out","sourceJar",f))
+		ensureDirectoryExists(os.path.join("out"+os.path.sep+"sourceJar",os.path.dirname(f)))
+		shutil.copy2(os.path.join("src", f), os.path.join("out"+os.path.sep+"sourceJar",f))
+	issueCommand(["jar", "cf", "out"+os.sep+"mtk-sources.jar", "-C", "out"+os.sep+"sourceJar", "."])
+tasks["sourceJar"] = sourceJar
+
+def javadoc(thingToDo=None):
+	if thingToDo == "description":
+		return "Generates the javadoc"
+	if thingToDo == "dependencies":
+		return []
+	files = find("src", ".java")
+	print "Found {} source files".format(len(files))
+	command = ["javadoc", "-d", "out"+os.sep+"javadoc"]
+	for f in files:
+		command.append(f)
+	issueCommand(command)
+tasks["javadoc"] = javadoc
+
+def javadocJar(thingToDo=None):
+	if thingToDo == "description":
+		return "Builds the javadoc jar."
+	if thingToDo == "dependencies":
+		return ["javadoc"]
+	issueCommand(["jar", "cf", "out"+os.sep+"mtk-javadoc.jar", "-C", "out"+os.sep+"javadoc", "."])
+tasks["javadocJar"] = javadocJar
+
+def jars(thingToDo=None):
+	if thingToDo == "description":
+		return "Builds all the jars"
+	if thingToDo == "dependencies":
+		return ["binaryJar", "sourceJar", "javadocJar"]
+tasks["jars"] = jars
 
 def runTest(thingToDo=None):
 	if thingToDo == "description":
 		return "Runs the test class"
 	if thingToDo == "dependencies":
-		return ["buildTestClasses", "jar"]
+		return ["binaryJar", "buildTestClasses"]
 	libraries = find("lib", ".jar", ["javadoc.jar", "sources.jar"])
-	libraries.append("out/mtk.jar")
+	libraries.append("out"+os.sep+"mtk.jar")
 	print "Found {} libraries".format(len(libraries))
-	classpath = "out/classes/test"
+	classpath = "out"+os.sep+"classes"+os.sep+"test"
 	for lib in libraries:
 		classpath += os.pathsep
 		classpath += lib
